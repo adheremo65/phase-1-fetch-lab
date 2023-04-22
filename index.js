38c1715c89736da67d67fae855360e0f55d2b54a
Abdela umer <adheremo65@gmail.com>
2023-04-22 00:56:25 -0400

    Fetch lab is done

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function fetchBooks() {
+   return fetch("https://anapioficeandfire.com/api/books")
+   .then(res=>res.json())
+   .then((data=>renderBooks(data)))
+   //.then(data => renderBooks(data))}
+}
+
+  
+
+  
   // To pass the tests, don't forget to return your fetch!
   
-}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
